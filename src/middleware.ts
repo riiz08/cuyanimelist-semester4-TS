2e94341f9d8ca8a4f78110c1597ecf549f776b3f
riiz08 <mhmdrizki0812@gmail.com>
2024-09-06 11:32:35 +0700

    asdbhasbdsasas

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const session = req.cookies.get("__Secure-next-auth.session-token");
+  const session =
+    process.env.NODE_ENV === "production"
+      ? req.cookies.get("__Secure-next-auth.session-token")
+      : req.cookies.get("next-auth.session-token");
   if (!session && req.nextUrl.pathname.startsWith("/user")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
